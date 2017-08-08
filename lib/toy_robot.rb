138f670e539353fac5a43cc1e8e6df2908a2de41
Harry Lee <harry.lee@digivizer.com>
2017-08-08 23:31:23 +1000

    Add new methods and specs for toy_robot

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -1,40 +1,100 @@
 $LOAD_PATH << '.'
 
 require 'north'
+require 'east'
 
 class ToyRobot
-
-  def initialize(x, y, f, input)
-    @x = x
-    @y = y
-    @f = f
-    @input = input
+  def initialize
+    @coordinate = []
   end
 
-  def place()
+  def read_input
+    input_file = File.open("input.txt", "r")
+
+    input_file.each do |i|
+      if i.include? "PLACE"
+        input = i.split(" ")[1].split(",")
+       @coordinate = place(input)
+      elsif i.include? "Report"
+        p @coordinate
+      else
+        check_input(i.delete("\n"))
+      end
+    end
+  end
+
+  def place(input)
+    x = input[0].to_i 
+    y = input[1].to_i 
+    f = input[2]
     coordinate = []
-    if check_location(@x, @y)
-      move_robot(coordinate = [@x, @y, @f])
+
+    if check_location(x, y)
+      coordinate = [x, y, f]
     else
-      puts "Wrong coordinate. Please try again"
+      coordinate
     end
   end
 
   def check_location(x, y)
     if x * y > 25
       return false
+    elsif x > 5 || y > 5
+      return false
     else
       return true
     end
   end
 
-  def move_robot(coordinate)
-    first_direction = coordinate[2]
-
-    if first_direction == "North"
-      result = North.move(@input, coordinate)
+  def check_input(input)
+    if @coordinate.length > 0
+      if input == "Move"
+        move_robot
+      elsif input == "Left"
+        left_handler
+      elsif input == "Right"
+        right_handler
+      end
     end
-    
-    coordinate = result
   end
+
+  def move_robot
+    if @coordinate[2] == "North"
+      @coordinate[1] += 1 unless @coordinate[1] >= 5
+    elsif @coordinate[2] == "East"
+      @coordinate[0] += 1 unless @coordinate[0] >= 5
+    elsif @coordinate[2] == "West"
+      @coordinate[0] -= 1 unless @coordinate[0] <= 0
+    elsif @coordinate[2] == "South"
+      @coordinate[1] -= 1 unless @coordinate[1] <= 0
+    end
+    @coordinate
+  end
+
+  def left_handler
+    if @coordinate[2] == "North"
+      @coordinate[2] = "West"
+    elsif @coordinate[2] == "East"
+      @coordinate[2] = "North"
+    elsif @coordinate[2] == "West"
+      @coordinate[2] = "South"
+    elsif @coordinate[2] == "South"
+      @coordinate[2] = "East"
+    end
+    @coordinate
+  end
+
+  def right_handler
+    if @coordinate[2] == "North"
+      @coordinate[2] = "East"
+    elsif @coordinate[2] == "East"
+      @coordinate[2] = "South"
+    elsif @coordinate[2] == "West"
+      @coordinate[2] = "North"
+    elsif @coordinate[2] == "South"
+      @coordinate[2] = "West"
+    end
+    @coordinate
+  end
+
 end
--- spec/toy_robot_spec.rb
+++ spec/toy_robot_spec.rb
@@ -2,35 +2,94 @@ require 'toy_robot'
 
 describe ToyRobot do
   before do
-    @robot = ToyRobot.new(2, 3, "North", ["Move"])
+    @robot = ToyRobot.new()
+    @robot_with_report = ToyRobot.new()
+    @robot_wrong_coordinate = ToyRobot.new()
+    @robot_out_of_range = ToyRobot.new()
   end
 
   let(:direction) { "North" }
-  let(:coordinate) { [2, 4, "North"]}
+  let(:input) { ["Move", "PLACE 2,2,North", "Move", "Right", "Move", "Report"] }
+  let(:coordinate1) { [2, 2, "North"]}
+  let(:coordinate) { [3, 4, "East"]}
+  let(:coordinate_with_report) { [3, 2, "North"]}
+  let(:coordinate_with_out_of_range) { [4, 5, "West"]}
 
-  describe "place" do
-    it "set starting point" do
-      expect(@robot.place).to eql(coordinate)
+  describe "read_input" do
+    it "reads input file" do
+      expect(@robot.place([2, 2, "North"])).to eql(coordinate1)
     end
   end
 
   describe "place" do
-    xit "set starting point" do
-      expect(ToyRobot.this_place(2, 4, direction)).to eql(true)
+    it "sets coordinate when an input from file contains PLACE" do
+      expect(@robot.place([2, 2, "North"])).to eql(coordinate1)
     end
 
-    context "move a toy" do
-      xit "and reports its current position" do
-        expect(ToyRobot.move(2, 4)).to eql(0)
-      end
+    it "does not sets coordinate when an input is out of range" do
+      expect(@robot.place([6, 4, "North"])).to eql([])
     end
+
+#    it "reports its current position" do
+#      expect(@robot_with_report.place).to eql(coordinate_with_report) 
+#    end
+#    it "does not move robot out of the table" do
+#      expect(@robot_out_of_range.place).to eql(coordinate_with_out_of_range)
+#    end
   end
 
   describe "check_location" do
-    context "checks location and does not move a toy" do
-      xit "when it is out of given table" do
-        expect(ToyRobot.this_check_location(4, 7)).to eql(false)
-      end
+    it "return false when a coordinate is out of the table" do
+      expect(@robot.check_location(6, 7)).to eql false
+    end
+
+    it "return false when a coordinate is out of the table" do
+      expect(@robot.check_location(6, 0)).to eql false
+    end
+
+    it "return false when a coordinate is out of the table" do
+      expect(@robot.check_location(0, 6)).to eql false
+    end
+
+    it "return true when initial coordinate is within the table" do
+      expect(@robot.check_location(0, 0)).to eql true
     end
   end
+
+  describe "move_robot" do
+    it "moves robot based on where it facing" do
+      @robot.instance_variable_set(:@coordinate, [3, 2, "North"])
+      expect(@robot.move_robot).to eql [3, 3, "North"]
+    end
+
+    it "moves robot based on where it facing" do
+      @robot.instance_variable_set(:@coordinate, [3, 2, "East"])
+      expect(@robot.move_robot).to eql [4, 2, "East"]
+    end
+  end
+
+  describe "left_handler" do
+    it "changes direction based on current facing direction" do
+      @robot.instance_variable_set(:@coordinate, [3, 2, "North"])
+      expect(@robot.left_handler).to eql [3, 2, "West"]
+    end
+
+    it "changes direction based on current facing direction" do
+      @robot.instance_variable_set(:@coordinate, [3, 2, "East"])
+      expect(@robot.left_handler).to eql [3, 2, "North"]
+    end
+  end
+
+  describe "right_handler" do
+    it "changes direction based on current facing direction" do
+      @robot.instance_variable_set(:@coordinate, [3, 2, "North"])
+      expect(@robot.right_handler).to eql [3, 2, "East"]
+    end
+
+    it "changes direction based on current facing direction" do
+      @robot.instance_variable_set(:@coordinate, [3, 2, "East"])
+      expect(@robot.right_handler).to eql [3, 2, "South"]
+    end
+  end
+
 end
